a90b5be67ac071a73b46b0c3f7d5e4c196d7be89
Iustin Andrei Bacaila <146625547+ShyexEE@users.noreply.github.com>
2024-02-19 18:52:28 +0000

    Update index.js Removed Host URL

--- index.js
+++ index.js
@@ -533,7 +533,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://ashopp.onrender.com/auth/google/login",
+      callbackURL: "/auth/google/login",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
